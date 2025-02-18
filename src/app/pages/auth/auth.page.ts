0d04a99b94fb419098a30d9e09eb84215208591e
jcapitanmoreno <jcapitanmoreno@gmail.com>
2025-02-18 11:28:36 +0100

    integracion con firebase pendiente

--- src/app/pages/auth/auth.page.ts
+++ src/app/pages/auth/auth.page.ts
@@ -1,20 +1,61 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { IonContent, IonIcon } from '@ionic/angular/standalone';
+import { HeaderComponent } from 'src/app/shared/components/header/header.component';
+import { CustomInputComponent } from 'src/app/shared/components/custom-input/custom-input.component';
+import { addIcons } from 'ionicons';
+import {
+  lockClosedOutline,
+  mailOutline,
+  personAddOutline,
+  logInOutline,
+} from 'ionicons/icons';
+import { IonButton } from '@ionic/angular/standalone';
+import { LogoComponent } from 'src/app/shared/components/logo/logo.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.page.html',
   styleUrls: ['./auth.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonIcon,
+    HeaderComponent,
+    IonContent,
+    CommonModule,
+    FormsModule,
+    CustomInputComponent,
+    ReactiveFormsModule,
+    IonButton,
+    LogoComponent,
+    RouterLink
+  ],
 })
 export class AuthPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
+  constructor() {
+    addIcons({
+      mailOutline,
+      lockClosedOutline,
+      personAddOutline,
+      logInOutline,
+    });
   }
 
+  ngOnInit() {}
+
+  submit() {
+    console.log(this.form.value);
+  }
 }
--- src/app/shared/components/logo/logo.component.ts
+++ src/app/shared/components/logo/logo.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { IonText } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-logo',
   templateUrl: './logo.component.html',
   styleUrls: ['./logo.component.scss'],
+  standalone: true,
+  imports: [IonText]
 })
 export class LogoComponent  implements OnInit {
 
